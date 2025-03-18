c33ea63357c99659ebb5c959d7da4d71a7ec4bb4
kayyraa <zindhukayrabasbay@gmail.com>
2025-03-18 22:02:12 +0300

    Cleared all files

--- scripts/controls.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("touchstart", (Event) => {
-    if (Event.touches.length > 1) Event.preventDefault();
-}, { passive: false });
-
-document.addEventListener("dblclick", (Event) => Event.preventDefault(), { passive: false });
